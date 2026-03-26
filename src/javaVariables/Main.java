12aa9028feaa46a199e25018d5551166eab11c65
sarvanan-prog <saravanan.mca1992@gmail.com>
2026-03-26 19:27:00 +0530

    variable basics

---
+++ src/javaVariables/Main.java
@@ -0,0 +1,11 @@
+package javaVariables;
+
+public class Main {
+    public static void main(String[] args) {
+        int firstNumber = 100;
+        int secondNumber = 500;
+        int result = firstNumber + secondNumber;
+        System.out.println("Result====>" + result);
+
+    }
+}
